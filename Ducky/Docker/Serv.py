9976fb6645a62aa1fe481edc3f2cc3888e503be3
Alexis Drago Beltran <alexis.drago-beltran@epitech.eu>
2024-12-08 19:28:24 +0100

    [UPDATE] server files

--- Ducky/Docker/Serv.py
+++ Ducky/Docker/Serv.py
@@ -1,15 +1,15 @@
-# server.py
-# Alexis.D 2k24
-
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/receive', methods=['POST'])
 def receive_data():
-    data = request.get_json()
-    print("Data received:", data)
-    return "Data received successfully", 200
+    data = request.json
+    if data:
+        print(f"Data received: {data}")
+        return {"status": "success", "message": "Data received"}, 200
+    else:
+        return {"status": "error", "message": "No data received"}, 400
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
